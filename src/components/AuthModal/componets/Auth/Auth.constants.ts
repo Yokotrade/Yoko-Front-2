e4b7d6f264a2acec79b3212e03cf4efb82cc7568
Ivan Sosnovich <isosnovich@yandex.ru>
2022-12-15 23:08:43 +0300

    ORG-19: Подключить Formik и настроить схемы валидации для авторизации и регистрации

---
+++ src/components/AuthModal/componets/Auth/Auth.constants.ts
@@ -0,0 +1,16 @@
+import * as Yup from 'yup'
+
+export interface AuthInitialValues {
+  email: string;
+  password: string;
+}
+
+export const initialValues: AuthInitialValues = {
+  email: "",
+  password: "",
+};
+
+export const validationSchema = Yup.object().shape({
+    email: Yup.string().required(),
+    password: Yup.string().required(),
+})
