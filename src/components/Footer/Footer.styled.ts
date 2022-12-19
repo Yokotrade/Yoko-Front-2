4593060ff077804b18bf6070d2aa7a8007f192b7
Ivan Sosnovich <isosnovich@yandex.ru>
2022-12-19 15:03:49 +0300

    ORG-26: footer

---
+++ src/components/Footer/Footer.styled.ts
@@ -0,0 +1,35 @@
+import styled from "@emotion/styled";
+import { start } from "repl";
+
+export const FooterWrapper = styled.footer`
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  padding: 50px 50px;
+  gap: 228px;
+  width: 1920px;
+  background: #181938;
+  box-sizing: border-box;
+`;
+
+export const PoliticsBlock = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+export const SectionsBlock = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: flex-start;
+  gap: 20px;
+`;
+
+export const SocialBlock = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  gap: 28px;
+`;
