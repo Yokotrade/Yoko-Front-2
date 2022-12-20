4661d888ef7b7b50f4bc76884cca7d5c4d5213da
Ivan Sosnovich <isosnovich@yandex.ru>
2022-12-20 20:58:43 +0300

    ORG-32: Правки перевода главной лендинга

--- src/pages/General/General.constants.ts
+++ src/pages/General/General.constants.ts
@@ -5,7 +5,7 @@ import { CardContentProps } from "components/CardContent";
 export const generalCards: CardContentProps[] = [
   {
     Icon: WalletIcon,
-    title: "account_balance",
+    title: "cardContent.account_balance",
     description: "10.01 USDT",
     actions: [
       { title: "cardContent.replenish" },
